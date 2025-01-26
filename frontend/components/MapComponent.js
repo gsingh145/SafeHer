43437ba35ced3d279d0014164360f43c0045b00d
Gaurav Singh <singh975@purdue.edu>
2025-01-25 22:37:53 -0500

    Add frontend/components/MapComponent.js

---
+++ frontend/components/MapComponent.js
@@ -0,0 +1,50 @@
+// components/MapComponent.js
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { styles } from '../styles/styles';
+import { GO_IP } from '../config'; // Import the IP address from config.js
+
+const MapComponent = ({ filterCategory }) => {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    const fetchMarkers = async () => {
+      try {
+        const category = filterCategory === 'All' ? null : filterCategory; 
+        const url = category 
+          ? `http://${GO_IP}/incidents/filter?category=${filterCategory}`
+          : `http://${GO_IP}/incidents`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setMarkers(data);
+      } catch (error) {
+        console.error('Error fetching markers:', error);
+      }
+    };
+
+    fetchMarkers();
+  }, [filterCategory]); // Refetch whenever filterCategory changes
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 40.4250,
+        longitude: -86.9230,
+        latitudeDelta: 0.035,
+        longitudeDelta: 0.035,
+      }}
+    >
+      {markers.map((marker, index) => (
+        <Marker
+          key={index}
+          coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+          title={marker.title}
+          description={marker.description}
+        />
+      ))}
+    </MapView>
+  );
+};
+
+export default MapComponent;
